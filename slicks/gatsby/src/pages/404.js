1ee43579abe35108a42cf4a826d07e8ac4a3f04e
Hayden Liddiard <cid196420@gmail.com>
2020-10-09 12:27:25 -0700

    initial commit

---
+++ slicks/gatsby/src/pages/404.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import FourOhFourPage from '../../../../master-gatsby-master/finished-files/gatsby/src/pages/404';
+
+export default function FourOhFourPage() {
+    return <p>Oooops! - No page here.</p>
+}
