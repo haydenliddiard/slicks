55c9bbba0da03b47a16b6677005b17da12b1cdd2
Hayden Liddiard <cid196420@gmail.com>
2020-10-15 10:57:41 -0700

    added saniy sourcing

--- slicks/gatsby/gatsby-config.js
+++ slicks/gatsby/gatsby-config.js
@@ -1,7 +1,28 @@
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env' });
+
+// never do this on build, only for test
+// console.log(process.env.SANITY_TOKEN)
+
 export default {
     siteMetadata: {
         title: `Slicks Slices`,
         siteUrl: 'https://gatsby-pizza',
         description: 'pizza so good it\'ll melt your face off.',
+    },
+   plugins: [
+    'gatsby-plugin-styled-components',
+    {
+        // name of the plugin your adding
+        resolve: 'gatsby-source-sanity',
+        options: {
+            projectId: 'romuqopg',
+            dataset: 'slicks',
+            watchMode: true,
+            token: process.env.SANITY_TOKEN,
+            
+        }
     }
-}
+   ], 
+};
