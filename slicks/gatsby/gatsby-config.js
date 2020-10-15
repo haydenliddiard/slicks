ee97af6b1ccb8bbc90457c1b9047cdb4ca945a77
Hayden Liddiard <cid196420@gmail.com>
2020-10-15 10:23:33 -0700

    gatsby config

--- slicks/gatsby/gatsby-config.js
+++ slicks/gatsby/gatsby-config.js
@@ -1,1 +1,7 @@
-
+export default {
+    siteMetadata: {
+        title: `Slicks Slices`,
+        siteUrl: 'https://gatsby-pizza',
+        description: 'pizza so good it\'ll melt your face off.',
+    }
+}
