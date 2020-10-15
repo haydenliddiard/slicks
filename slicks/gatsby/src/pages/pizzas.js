39bed9fa7d8a202d88d29d726da9c4251b874c88
Hayden Liddiard <cid196420@gmail.com>
2020-10-15 15:28:26 -0700

    halfway through 18th vid copied code from file

--- slicks/gatsby/src/pages/pizzas.js
+++ slicks/gatsby/src/pages/pizzas.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+    const pizzas = data.pizzas.nodes;
+
     return (
         <>
-            pizzas
+            <p>Hey! There are {pizzas.length} pizza's</p>
+            <PizzaList pizzas={pizzas} />
         </>
     )
 }
+
+export const query = graphql`
+    query PizzaQuery {
+        pizzas: allSanityPizza {
+            nodes {
+                name
+                id
+                slug {
+                    current
+                }
+                toppings {
+                    id
+                    name
+                }
+                image {
+                    asset {
+                        fluid(maxWidth: 400) {
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`; 
