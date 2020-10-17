a124139b705f358d2f9c63da9ca977bcbafcb040
Hayden Liddiard <cid196420@gmail.com>
2020-10-16 17:05:58 -0700

    finished grid layout  css grid

--- slicks/gatsby/src/components/PizzaList.js
+++ slicks/gatsby/src/components/PizzaList.js
@@ -1,27 +1,51 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const PizzaGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaStyles = styled.div`
+  display: grid;
+  /*take your row sizing not from the pizzastyles but from PizzaGridStyles */
+  /*subgrid doesnt work on chrome*/
+  @supports not (grid-template-rows: subgrid;) {
+    grid-template-rows: auto auto 1fr;
+  }
+  grid-template-rows: subgrid;
+  grid-row: span 3;
+  gap: 1rem;
+  h2, p {
+    margin: 0;
+
+  }
+`;
 
 function SinglePizza({ pizza }) {
   return (
-    <div>
+    <PizzaStyles>
       <Link to={`/pizza/${pizza.slug.current}`}>
         <h2>
           <span className="mark">{pizza.name}</span>
         </h2>
+      </Link>
         <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
         <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
-      </Link>
-    </div>
+    </PizzaStyles>
   );
 }
 
 export default function PizzaList({ pizzas }) {
   return (
-    <div>
+    <PizzaGridStyles>
       {pizzas.map((pizza) => (
         <SinglePizza key={pizza.id} pizza={pizza} />
       ))}
-    </div>
+    </PizzaGridStyles>
   );
 }
